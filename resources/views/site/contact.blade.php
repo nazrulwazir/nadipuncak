6e07ea0e3c3c5c80dbb159528a1eaefc1fa6c55c
nazrul <nazrulhakimwazir@gmail.com>
2024-08-13 23:36:42 +0800

    up

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -1,16 +1,16 @@
 @extends('site.layouts.app')
 
-@section('title', 'Contact')
+@section('title', __('contact.title'))
 
 @section('content')
-    @include('site.partials.page-header', ['title' => 'Contact Us'])
+    @include('site.partials.page-header', ['title' => __('contact.header_title')])
 
     <!-- Contact Start -->
     <div class="contact wow fadeInUp">
         <div class="container">
             <div class="section-header text-center">
-                <p>Get In Touch</p>
-                <h2>For Any Query</h2>
+                <p>{{ __('contact.get_in_touch') }}</p>
+                <h2>{{ __('contact.query') }}</h2>
             </div>
             <div class="row">
                 <div class="col-md-6">
@@ -18,21 +18,21 @@
                         <div class="contact-item">
                             <i class="flaticon-address"></i>
                             <div class="contact-text">
-                                <h2>Location</h2>
-                                <p>No. 283, Tingkat 1, Jalan Shahab Perdana, 05150 Alor Setar, Kedah Darul Aman, Malaysia</p>
+                                <h2>{{ __('contact.location') }}</h2>
+                                <p>{{ __('contact.address') }}</p>
                             </div>
                         </div>
                         <div class="contact-item">
                             <i class="flaticon-call"></i>
                             <div class="contact-text">
-                                <h2>Phone</h2>
+                                <h2>{{ __('contact.phone') }}</h2>
                                 <p>04 - 787 7661</p>
                             </div>
                         </div>
                         <div class="contact-item">
                             <i class="flaticon-send-mail"></i>
                             <div class="contact-text">
-                                <h2>Email</h2>
+                                <h2>{{ __('contact.email') }}</h2>
                                 <p>nadipuncak23@gmail.com</p>
                             </div>
                         </div>
@@ -45,23 +45,23 @@
                             <input type="hidden" name="access_key" value="c32832e4-d3ef-4c73-af27-cf1a81d2c67f">
 
                             <div class="control-group">
-                                <input type="text" class="form-control" name="name" placeholder="Your Name" required />
+                                <input type="text" class="form-control" name="name" placeholder="{{ __('contact.your_name') }}" required />
                                 <p class="help-block text-danger"></p>
                             </div>
                             <div class="control-group">
-                                <input type="email" class="form-control" name="email" placeholder="Your Email" required />
+                                <input type="email" class="form-control" name="email" placeholder="{{ __('contact.your_email') }}" required />
                                 <p class="help-block text-danger"></p>
                             </div>
                             <div class="control-group">
-                                <input type="text" class="form-control" name="subject" placeholder="Subject" required />
+                                <input type="text" class="form-control" name="subject" placeholder="{{ __('contact.subject') }}" required />
                                 <p class="help-block text-danger"></p>
                             </div>
                             <div class="control-group">
-                                <textarea class="form-control" name="message" placeholder="Message" required></textarea>
+                                <textarea class="form-control" name="message" placeholder="{{ __('contact.message') }}" required></textarea>
                                 <p class="help-block text-danger"></p>
                             </div>
                             <div>
-                                <button class="btn btn-primary" type="submit">Send Message</button>
+                                <button class="btn btn-primary" type="submit">{{ __('contact.send_message') }}</button>
                             </div>
                         </form>
                     </div>
@@ -87,11 +87,11 @@
 
             if (result.success) {
                 // Display success message
-                alert('Your message has been sent successfully!');
+                alert('{{ __('contact.success_message') }}');
                 form.reset();  // Reset the form
             } else {
                 // Display error message
-                alert('There was an error sending your message. Please try again later.');
+                alert('{{ __('contact.error_message') }}');
             }
         });
     });
