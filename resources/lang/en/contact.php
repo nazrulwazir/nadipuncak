2f14a7ee637a5daf11a6f04b580b0e3746e7921a
nazrul <nazrulhakimwazir@gmail.com>
2024-08-13 23:37:23 +0800

    up

---
+++ resources/lang/en/contact.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Contact',
+    'header_title' => 'Contact Us',
+    'get_in_touch' => 'Get In Touch',
+    'query' => 'For Any Query',
+    'location' => 'Location',
+    'address' => 'No. 283, Tingkat 1, Jalan Shahab Perdana, 05150 Alor Setar, Kedah Darul Aman, Malaysia',
+    'phone' => 'Phone',
+    'email' => 'Email',
+    'send_message' => 'Send Message',
+    'your_name' => 'Your Name',
+    'your_email' => 'Your Email',
+    'subject' => 'Subject',
+    'message' => 'Message',
+    'success_message' => 'Your message has been sent successfully!',
+    'error_message' => 'There was an error sending your message. Please try again later.',
+];
---
+++ resources/lang/ms/contact.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Hubungi',
+    'header_title' => 'Hubungi Kami',
+    'get_in_touch' => 'Hubungi Kami',
+    'query' => 'Untuk Sebarang Pertanyaan',
+    'location' => 'Lokasi',
+    'address' => 'No. 283, Tingkat 1, Jalan Shahab Perdana, 05150 Alor Setar, Kedah Darul Aman, Malaysia',
+    'phone' => 'Telefon',
+    'email' => 'E-mel',
+    'send_message' => 'Hantar Mesej',
+    'your_name' => 'Nama Anda',
+    'your_email' => 'E-mel Anda',
+    'subject' => 'Subjek',
+    'message' => 'Mesej',
+    'success_message' => 'Mesej anda telah berjaya dihantar!',
+    'error_message' => 'Terdapat ralat semasa menghantar mesej anda. Sila cuba lagi kemudian.',
+];
