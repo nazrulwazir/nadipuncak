2019e67f4621104dab63294ca7ae19e6e4f00e43
nazrul <nazrulhakimwazir@gmail.com>
2024-08-14 12:34:35 +0800

    up

--- resources/views/site/partials/fact.blade.php
+++ resources/views/site/partials/fact.blade.php
@@ -1,11 +1,11 @@
 <!-- resources/views/site/partials/fact.blade.php -->
 <div class="fact service-section">
     <div class="container-fluid">
-        <div class="row counters">
-            <div class="col-md-6 fact-left wow slideInLeft">
-                <div class="row">
-                    <div class="col-6">
-                        <div class="fact-icon">
+        <div class="row counters justify-content-center">
+            <div class="col-md-6 fact-left wow slideInLeft d-flex justify-content-center">
+                <div class="row w-100">
+                    <div class="col-12 col-sm-6 d-flex flex-column align-items-center text-center mb-4">
+                        <div class="fact-icon mb-2">
                             <i class="flaticon-worker"></i>
                         </div>
                         <div class="fact-text">
@@ -13,8 +13,8 @@
                             <p>{{ __('fact.expert_workers') }}</p>
                         </div>
                     </div>
-                    <div class="col-6">
-                        <div class="fact-icon">
+                    <div class="col-12 col-sm-6 d-flex flex-column align-items-center text-center mb-4">
+                        <div class="fact-icon mb-2">
                             <i class="flaticon-building"></i>
                         </div>
                         <div class="fact-text">
@@ -24,10 +24,10 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-6 fact-right wow slideInRight">
-                <div class="row">
-                    <div class="col-6">
-                        <div class="fact-icon">
+            <div class="col-md-6 fact-right wow slideInRight d-flex justify-content-center">
+                <div class="row w-100">
+                    <div class="col-12 col-sm-6 d-flex flex-column align-items-center text-center mb-4">
+                        <div class="fact-icon mb-2">
                             <i class="flaticon-address"></i>
                         </div>
                         <div class="fact-text">
@@ -35,8 +35,8 @@
                             <p>{{ __('fact.completed_projects') }}</p>
                         </div>
                     </div>
-                    <div class="col-6">
-                        <div class="fact-icon">
+                    <div class="col-12 col-sm-6 d-flex flex-column align-items-center text-center mb-4">
+                        <div class="fact-icon mb-2">
                             <i class="flaticon-crane"></i>
                         </div>
                         <div class="fact-text">
